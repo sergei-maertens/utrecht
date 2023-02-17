a86dee20a83a6c8078be6f3402df0451305f5b1d
Robbert <mail@robbertbroersma.nl>
2023-02-17 08:38:30 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@a344de9833fc1df25a13553d4a78f36b85a4191a 🚀

---
+++ storybook-react/assets/ButtonGroup.stories-b6c6f204.js
@@ -0,0 +1,16 @@
+import{a as c,F as u,j as n}from"./jsx-runtime-670450c2.js";import{B as s,c as r}from"./index-4b8344d7.js";import{t as p,d as l}from"./util-36ba316b.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./clsx.m-ace26a20.js";import"./extends-98964cd2.js";import"./index-4d501b15.js";const m=`<!-- @license CC0-1.0 -->
+
+# Button group
+
+De _button group_ is een container voor een aantal buttons, of voor een losstaande button.
+
+## Terminologie
+
+- **button**: van het \`<button>\` HTML element, \`role="button"\` in ARIA.
+- **group**: van \`role="group"\` in ARIA.
+
+## Verticale variant
+
+Voor smalle schermen bij plaatsing in smalle panelen (zoals een sidebar) is er een variant waar buttons onder elkaar staan, in plaats van naast elkaar. Gebruik deze variant niet wanneer er voldoende ruimte is voor de standaard variant. Je kunt bijvoorbeeld CSS media queries gebruiken om deze variant toe te passen voor kleine schermen.
+`,g={"button-group":{"background-color":{css:{syntax:"<color>",inherits:!0}},"column-gap":{css:{syntax:"<length>",inherits:!0},comment:"'column-gap', for lack of a better existing property"},"margin-block-end":{css:{syntax:"<length>",inherits:!0}},"margin-block-start":{css:{syntax:"<length>",inherits:!0}},"padding-block-end":{css:{syntax:"<length>",inherits:!0}},"padding-block-start":{css:{syntax:"<length>",inherits:!0}},"row-gap":{css:{syntax:"<length>",inherits:!0},comment:"'row-gap', for lack of a better existing property"}}},d={utrecht:g},b={title:"React Component/Button group",id:"react-button-group",component:s,argTypes:{},tags:["autodocs"],parameters:{tokensPrefix:"utrecht-button-group",tokens:p,tokensDefinition:d,docs:{description:{component:m}}}},h=i=>n(s,{...i}),e=h.bind({});e.args={children:c(u,{children:[n(r,{appearance:"primary-action-button",children:"Save and continue"}),n(r,{appearance:"secondary-action-button",children:"Back"})]})};const t=l(b);var o;e.parameters={...e.parameters,storySource:{source:"args => <ButtonGroup {...args} />",...(o=e.parameters)==null?void 0:o.storySource}};var a;t.parameters={...t.parameters,storySource:{source:"designTokenStory(meta)",...(a=t.parameters)==null?void 0:a.storySource}};const D=["Default","DesignTokens"];export{e as Default,t as DesignTokens,D as __namedExportsOrder,b as default};
+//# sourceMappingURL=ButtonGroup.stories-b6c6f204.js.map
---
+++ storybook-react/assets/PageHeader.stories-c50b99de.js
@@ -0,0 +1,25 @@
+import{j as t}from"./jsx-runtime-670450c2.js";import{_ as o,H as i}from"./index-4b8344d7.js";import{t as d,d as g}from"./util-36ba316b.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./clsx.m-ace26a20.js";import"./extends-98964cd2.js";import"./index-4d501b15.js";const l=`<!-- @license CC0-1.0 -->
+
+# Page Header
+
+De page header bevat vaak de volgende onderdelen:
+
+- Logo (met link naar de homepage)
+- Navigatie
+- Zoekveld
+- Kruimelpad-navigatie
+- Eventueel een melding met een waarschuwing, bijvoorbeeld over een actuele storing.
+- Cookie banner.
+
+Het logo maakt duidelijk wie verantwoordelijk is voor de website en geeft daarmee belangrijke context voor de rest van de pagina.
+
+## HTML
+
+Gebruik het HTML \`<header>\` element, zodat het automatisch een \`banner\` landmark krijgt.
+
+## Relevante WCAG eisen
+
+- [WCAG eis 1.3.1](https://www.w3.org/TR/WCAG21/#info-and-relationships): plaats de header in een \`banner\` landmark, door het \`<header>\` element te gebruiken
+- [WCAG eis 1.3.2](https://www.w3.org/TR/WCAG21/#meaningful-sequence): plaats de header aan het begin van pagina, maar na de _skip links_. Zo is het gelijk duidelijk wie verantwoordelijk is voor de pagina die er op volgt.
+`,c={"page-header":{"background-color":{css:{syntax:"<color>",inherits:!0}},color:{css:{syntax:"<color>",inherits:!0}},"padding-start":{css:{syntax:"<length>",inherits:!0}},"padding-block-end":{css:{syntax:"<length>",inherits:!0}}}},m={utrecht:c},p={title:"React Component/Page Header",id:"react-page-header",component:o,tags:["autodocs"],parameters:{tokensPrefix:"utrecht-page-header",tokens:d,tokensDefinition:m,docs:{description:{component:l}}}},h=s=>t(o,{...s}),e=h.bind({});e.args={children:t(i,{children:"Header Area"})};e.displayName="PageHeader";const a=g(p);var n;e.parameters={...e.parameters,storySource:{source:"args => <PageHeader {...args} />",...(n=e.parameters)==null?void 0:n.storySource}};var r;a.parameters={...a.parameters,storySource:{source:"designTokenStory(meta)",...(r=a.parameters)==null?void 0:r.storySource}};const j=["Default","DesignTokens"];export{e as Default,a as DesignTokens,j as __namedExportsOrder,p as default};
+//# sourceMappingURL=PageHeader.stories-c50b99de.js.map
