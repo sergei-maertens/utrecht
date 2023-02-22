ac7fb4aa769f156a92f0374d194e05b23ca7589c
Robbert <mail@robbertbroersma.nl>
2023-02-22 13:57:35 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@e39b53010fa5ad7ebdfed782bf9bd15587ff10f6 🚀

---
+++ storybook-react/assets/DataList.stories-f878a275.js
@@ -0,0 +1,110 @@
+import{j as t,a as o}from"./jsx-runtime-670450c2.js";import{m as s,n as d,o as l,p as m}from"./index-8b643457.js";import{t as u,d as g}from"./util-f18e0025.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./clsx.m-ace26a20.js";import"./extends-98964cd2.js";import"./index-4d501b15.js";const v=`<!-- @license CC0-1.0 -->
+
+# Data list
+
+De _data list_ component gebruik je onder andere voor:
+
+- een overzicht van gegevens uit een formulier, in een controle-stap voor het verzenden
+- een overzicht vooringevulde gegevens, aan het begin van een formulier
+- een overzicht van bekende gegevens, bijvoorbeeld in een profiel-pagina
+
+Om verschillende soorten data optimaal weer te geven, zijn er speciale componenten gemaakt:
+
+- URL Value: gebruik deze voor internetadressen en e-mailadressen
+- IBAN: gebruik deze voor dit type bankrekeningnummer
+
+Er zijn _BEM modifier class names_ beschikbaar van \`utrecht-data-list__value\`, voor datatypes:
+
+- \`multiline\`: voor gegevens die worden aangeleverd als \`string\`, maar waarvan de tekst verdeeld is over meerdere tekstregels zonder HTML \`<br/>\` maar met een _newline character_. Bijvoorbeeld ingevoerde tekst uit een \`textarea\`, of een adres.
+- \`number\`
+- \`telephone\`
+
+## Anatomie
+
+De _data list_ is opgebouwd uit de volgende onderdelen en BEM class names:
+
+- de _data list_
+  - _data list item_, één of meerdere. Naam is gebaseerd op "item" uit \`role="associationlistitemvalue"\` in de [WAI-ARIA 1.3 Draft](https://w3c.github.io/aria/#associationlistitemvalue)
+    - de _item key_. Naam is gebaseerd op het "key" in uit \`role="associationlistitemkey"\` in de [WAI-ARIA 1.3 Draft](https://w3c.github.io/aria/#associationlistitemkey).
+    - de _item value_. Naam is gebaseerd op \`value\` van het \`input\` HTML-element en op \`role="associationlistitemvalue"\` in de [WAI-ARIA 1.3 Draft](https://w3c.github.io/aria/#associationlistitemvalue).
+    - de _item actions_ (deze zijn optioneel)
+
+BEM class names:
+
+- \`utrecht-data-list\`. Gebruik ook \`utrecht-data-list--html-dl\` voor een reset van standaard styling van het \`<dl>\` element.
+- \`utrecht-data-list__item\` voor de \`<div>\`
+- \`utrecht-data-list__item-key\` voor de \`<dt>\`
+- \`utrecht-data-list__item-value\` voor de \`<dd>\`
+- \`utrecht-data-list__actions\` met een eigen \`<dd>\`
+
+## Vertaalde gegevens
+
+Kies per datatype of de _value_ in een andere taal vertaald moet zijn bij gebruikers van automatische vertaalsoftware zoals Google Translate. Van sommige gegevens moet je vertaling voorkomen, bijvoorbeeld bij namen van personen, straatnamen, codes zoals bankrekeningnummers, internetadressen en e-mailadressen. Gebruik het HTML-attribuut \`translate="no"\` voor deze gegevens.
+
+## HTML
+
+Gebruik in de HTML-elementen \`dl\`, \`dt\` en \`dd\` om ([WCAG eis 1.3.1](https://www.w3.org/TR/WCAG21/#info-and-relationships)) om duidelijk onderscheid te maken tussen _key_ en de _value_. Gebruik één \`div\` element om \`dt\` en \`dd\` te groeperen om met makkelijker met CSS de layout te maken.
+
+Je mag in theorie ook een \`table\` element met \`th\` voor de _key_ en \`td\` voor de _value_ gebruiken, maar alleen als je ook een tabel-layout gebruikt. Om de _data list_ leesbaar te maken op kleine schermen is het handiger om geen tabel te gebruiken.
+
+## Hoe het niet moet
+
+### Geen \`dl\` of \`table\` gebruiken
+
+Gebruik niet alleen \`div\`, \`span\` en \`p\` HTML-element omdat dit niet voldoende duidelijk is (WCAG eis 1.3.1).
+
+## Button of link tekst heeft geen context
+
+Niet:
+
+\`\`\`html
+<dl>
+  <div>
+    <dt>Naam</dt>
+    <dd>Jos de Vos</dd>
+    <dd><a href="./step-1">Wijzig</a></dd>
+  </div>
+  <div>
+    <dt>Woonplaats</dt>
+    <dd>Amsterdam</dd>
+    <dd><a href="./step-2">Wijzig</a></dd>
+  </div>
+</dl>
+\`\`\`
+
+Wel:
+
+\`\`\`html
+<dl>
+  <div>
+    <dt>Naam</dt>
+    <dd>Mees de Vos</dd>
+    <dd><a href="./edit" title="Wijzig naam">Wijzig</a></dd>
+  </div>
+  <div>
+    <dt>Woonplaats</dt>
+    <dd>Amsterdam</dd>
+    <dd><a href="./edit" title="Wijzig woonplaats">Wijzig</a></dd>
+  </div>
+</dl>
+\`\`\`
+
+## Links
+
+- [Summary list van GOV.UK Design System](https://design-system.service.gov.uk/components/summary-list/)
+- [MDN: \`<dl>\`: The Description List element](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dl)
+
+### Relevante WCAG-eisen
+
+- [WCAG eis 1.3.1](https://www.w3.org/TR/WCAG21/#info-and-relationships) voor gebruik van semantische HTML
+- [WCAG eis 2.4.4](https://www.w3.org/TR/WCAG21/#link-purpose-in-context) en [WCAG eis 2.4.9](https://www.w3.org/TR/WCAG21/#link-purpose-link-only) voor duidelijke namen van acties bij een data list item
+- [WCAG eis 2.5.3](https://www.w3.org/TR/WCAG21/#label-in-name): als de zichtbare tekst van een action "Wijzig" is, dan moet het volledige label ook "Wijzig" gebruiken, dus "Wijzig voornaam" en niet "Bewerk voornaam".
+- [WCAG eis 3.2.4](https://www.w3.org/TR/WCAG21/#consistent-identification): en _data list_ voor controle van formulier-invoer moet consistent zijn bij de tekst van het formulier-label en de tekst van de _data list item key_.
+- [WCAG eis 3.3.4](https://www.w3.org/TR/WCAG21/#error-prevention-legal-financial-data) en [WCAG eis 3.3.6](https://www.w3.org/TR/WCAG21/#error-prevention-all): de data list gebruik je voor formulieren zodat gebruikers de invoer kunnen controleren
+`,c={"data-list":{"margin-block-end":{css:{syntax:"<length>",inherits:!0}},"margin-block-start":{css:{syntax:"<length>",inherits:!0}},"item-key":{color:{css:{syntax:"<color>",inherits:!0}},"font-size":{css:{syntax:"<length>",inherits:!0}},"font-weight":{css:{syntax:"<number>",inherits:!0}},"line-height":{css:{syntax:"<length>",inherits:!0}}},"item-value":{"font-size":{css:{syntax:"<length>",inherits:!0}},"line-height":{css:{syntax:"<length>",inherits:!0}}},rows:{item:{"margin-block-start":{css:{syntax:"<length>",inherits:!0}}},"item-value":{"margin-block-start":{css:{syntax:"<length>",inherits:!0}}}}}},h={utrecht:c},p={title:"React Component/Data List",id:"react-data-list",component:s,tags:["autodocs"],parameters:{tokensPrefix:"utrecht-data-list",tokens:u,tokensDefinition:h,docs:{description:{component:v}}}},k=r=>t(s,{...r,children:o(d,{children:[t(l,{children:"Gereserveerde zitplaats"}),t(m,{children:"42"})]})}),e=k.bind({});e.args={appearance:"rows"};const n=g(p);var a;e.parameters={...e.parameters,storySource:{source:`args => <DataList {...args}>
+    <DataListItem>
+      <DataListKey>Gereserveerde zitplaats</DataListKey>
+      <DataListValue>42</DataListValue>
+    </DataListItem>
+  </DataList>`,...(a=e.parameters)==null?void 0:a.storySource}};var i;n.parameters={...n.parameters,storySource:{source:"designTokenStory(meta)",...(i=n.parameters)==null?void 0:i.storySource}};const A=["Default","DesignTokens"];export{e as Default,n as DesignTokens,A as __namedExportsOrder,p as default};
+//# sourceMappingURL=DataList.stories-f878a275.js.map
---
+++ storybook-react/assets/Icon.stories-11be6a24.js
@@ -0,0 +1,86 @@
+import{j as n}from"./jsx-runtime-670450c2.js";import{M as c}from"./index-8b643457.js";import{t as l,d as u}from"./util-f18e0025.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./clsx.m-ace26a20.js";import"./extends-98964cd2.js";import"./index-4d501b15.js";const d=`<!-- @license CC0-1.0 -->
+
+# Iconen in code
+
+## Iconen gebruiken als Custom Element
+
+Eén van de makkelijkste manieren om iconen te gebruiken is door ze te includen in de web component library, zodat je ze als custom element kan gebruiken. Elk icoon is beschikbaar als custom element. De naam van van het icoon (\`utrecht-icon-...\`) is ook de naam van het custom-element. Voorbeeld:
+
+\`\`\`html
+<div>
+  <utrecht-icon-loupe></utrecht-icon-loupe>
+</div>
+\`\`\`
+
+Verander de kleur van het icoon via de CSS-eigenschap \`color\` op een parent element of via de custom eigenschap \`--utrecht-icon-color\`.
+
+### \`color\` van de omgevings content
+
+Op onderstaande manier kan je het makkelijkst de kleur aanpassen. De kleur van de bestaande tekst zal ook op het icoon toegepast worden.
+
+\`\`\`html
+<button>
+  <utrecht-icon-loupe></utrecht-icon-loupe>
+  Search
+</button>
+
+<style>
+  button {
+    background-color: rgb(0, 0, 0);
+    color: rgb(255, 255, 255);
+  }
+</style>
+\`\`\`
+
+### \`--utrecht-icon-color\` als custom icoon kleur
+
+Deze manier is handig als je een klein verschil wilt aanbrengen tussen de tekst kleur en de kleur van het icoon. Het gebruikt een CSS custom property \`--utrecht-icon-color\` als de kleur van het icoon.
+
+\`\`\`html
+<button>
+  <utrecht-icon-loupe></utrecht-icon-loupe>
+  Search
+</button>
+
+<style>
+  button {
+    --utrecht-icon-color: rgba(255, 255, 255, 0.75);
+    background-color: rgb(0, 0, 0);
+    color: rgb(255, 255, 255);
+  }
+</style>
+\`\`\`
+
+### Icoon grootte aanpassen
+
+De grootte van het icoon kan aangepast worden met de \`--utrecht-icon-size\` CSS eigenschap:
+
+\`\`\`html
+<button>
+  <utrecht-icon-loupe></utrecht-icon-loupe>
+  Search
+</button>
+
+<style>
+  button {
+    --utrecht-icon-size: 42px;
+    background-color: rgb(0, 0, 0);
+    color: rgb(255, 255, 255);
+  }
+</style>
+\`\`\`
+
+## Overzicht van icons
+
+Wanneer het nodig is een overzicht te hebben van de beschikbare icons, gebruik dan als basis de meegeleverde JSON informatie in \`dist/index.json\`. Het bevat een array met onder andere de ID van elke icoon. Op dit moment is de naam van de custom element altijd gelijk aan de ID.
+`,h={icon:{color:{css:{syntax:"<color>",inherits:!0}},size:{css:{syntax:"<length-percentage>",inherits:!0}},"inset-block-start":{css:{syntax:"<length>",inherits:!0}},baseline:{"inset-block-start":{css:{syntax:"<length>",inherits:!0}}}}},m={utrecht:h},p={title:"React Component/Icon",id:"react-icon",component:c,parameters:{tokensPrefix:"utrecht-icon",tokens:l,tokensDefinition:m,docs:{description:{component:d}}}},g=({color:i,size:s,...a})=>n("div",{style:{"--utrecht-icon-color":i,"--utrecht-icon-size":`${s}px`},children:n(c,{...a})}),e=g.bind({});e.args={children:n("svg",{viewBox:"0 0 1000 1000",children:n("rect",{fill:"currentColor",x:"0",y:"0",width:"1000",height:"1000"})}),color:"green",size:32};const t=u(p);var o;e.parameters={...e.parameters,storySource:{source:`({
+  color,
+  size,
+  ...args
+}) => <div style={{
+  '--utrecht-icon-color': color,
+  '--utrecht-icon-size': \`\${size}px\`
+}}>
+    <Icon {...args} />
+  </div>`,...(o=e.parameters)==null?void 0:o.storySource}};var r;t.parameters={...t.parameters,storySource:{source:"designTokenStory(meta)",...(r=t.parameters)==null?void 0:r.storySource}};const I=["WithInlineSVG","DesignTokens"];export{t as DesignTokens,e as WithInlineSVG,I as __namedExportsOrder,p as default};
+//# sourceMappingURL=Icon.stories-11be6a24.js.map
