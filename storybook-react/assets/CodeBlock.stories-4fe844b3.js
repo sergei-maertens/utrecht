ea8a7bdb79532d585125890b3283397e34c7648e
Robbert <mail@robbertbroersma.nl>
2023-03-17 10:51:58 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@844a50a4954e2448f7d2ad2a06a0b82d7816db1e 🚀

---
+++ storybook-react/assets/CodeBlock.stories-4fe844b3.js
@@ -0,0 +1,15 @@
+import{j as d}from"./jsx-runtime-670450c2.js";import{l as a}from"./index-8b643457.js";import{t as p,d as m}from"./util-7bd4bfc8.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./clsx.m-ace26a20.js";import"./extends-98964cd2.js";import"./index-4d501b15.js";const g=`<!-- @license CC0-1.0 -->
+
+# Code block
+`,y={"code-block":{"background-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},color:{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"font-family":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"*",inherits:!0}}},"font-size":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"line-height":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"margin-block-start":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"margin-block-end":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"margin-inline-start":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"margin-inline-end":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-block-start":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-block-end":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-inline-start":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-inline-end":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}}}},h={utrecht:y},x={title:"React Component/Code block",id:"react-code-block",component:a,tags:["autodocs"],parameters:{tokensPrefix:"utrecht-code-block",tokens:p,tokensDefinition:h,docs:{description:{component:g}}}},u=c=>d(a,{...c}),e=u.bind({});e.args={children:`<!DOCTYPE html>
+  <html lang="nl" dir="ltr">
+    <head>
+      <title>NL Design System</title>
+      <meta charset="utf-8"/>
+    </head>
+    <body>
+      <h1>NL Design System</h1>
+    </body>
+  </html>
+  `};const s=m(x);var t,n,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:"args => <CodeBlock {...args} />",...(r=(n=e.parameters)==null?void 0:n.docs)==null?void 0:r.source}}};var o,i,l;s.parameters={...s.parameters,docs:{...(o=s.parameters)==null?void 0:o.docs,source:{originalSource:"designTokenStory(meta)",...(l=(i=s.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};const j=["Default","DesignTokens"];export{e as Default,s as DesignTokens,j as __namedExportsOrder,x as default};
+//# sourceMappingURL=CodeBlock.stories-4fe844b3.js.map
