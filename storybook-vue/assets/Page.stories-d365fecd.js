0ae05eaa11ba622f67288b8c5847ca5f8c8c917d
Robbert <mail@robbertbroersma.nl>
2023-03-21 17:39:56 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@df5117aac5cfb571100c80f1dccee987de6a182d 🚀

---
+++ storybook-vue/assets/Page.stories-d365fecd.js
@@ -0,0 +1,59 @@
+import{I as t,K as i,W as r,J as s,Q as p,c as l}from"./util-e186a036.js";import"./vue.esm-bundler-e07e3e6f.js";import"./index-2812c619.js";import"./_commonjsHelpers-725317a4.js";import"./chunk-PCJTTTQV-457a1bfc.js";import"./preload-helper-41c905a7.js";import"./chunk-Y5O7ZP4P-aaefc2f2.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./index-f78bb811.js";import"./index-d475d2ea.js";import"./memoizerific-a8c4a000.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-bf486984.js";import"./chunk-RDJSMFWU-fd275764.js";import"./index-d37d4223.js";import"./index-0c05a4b7.js";import"./isSymbol-1aea413c.js";import"./index-356e4a49.js";import"./chunk-R4NKYYJA-96bb58e6.js";const m=`<!-- @license CC0-1.0 -->
+
+# Page component
+
+Component that centers the page. The page maximizes the width of the content for large viewports, to keep the line length of texts readable and avoid large horizontal distances between related components.
+
+Typically the contents of the page component are:
+
+- Page Header component
+- Page Content component
+  - Main content
+  - Complimentary content
+- Page Footer component
+
+## API
+
+- CSS class name: \`utrecht-page\`
+- Web component: \`<utrecht-page>\`
+- Angular/React/Vue.js component: \`UtrechtPage\`
+
+## CSS implementation
+
+### Margin
+
+To create some transparent space around the page, \`margin-inline\` CSS variables are provided. However, to automatically horizontally center the page \`margin-inline: auto\` needs to be used.
+
+Achieving both goals is made possible by implementing the \`margin-inline\` as \`padding-inline\` on a transparent container element. The only downside to this approach is that margin collapsing won't work anymore, but that doesn't seem like anyone would ever need for a component that realistically is only used once per document.
+
+### Padding
+
+Padding is not applied directly in the page component itself, but in the child components such as Page Header, Page Content and Page Footer.
+
+## Relevante WCAG regels
+
+- [WCAG eis 2.4.1](https://www.w3.org/TR/WCAG21/#bypass-blocks): gebruik de skip link zodat gebruikers direct naar de _main page content_ kunnen gaan.
+- [WCAG eis 2.4.2](https://www.w3.org/TR/WCAG21/#page-titled): de pagina moet een duidelijke titel hebben
+`,c={page:{"background-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},color:{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"margin-inline-start":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"margin-inline-end":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"max-inline-size":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}}}},d={utrecht:c},g={id:"vue-page",title:"Vue.js Component/Page",component:t,tags:["autodocs"],argTypes:{},args:{},render:()=>({components:{Page:t,PageHeader:i,PageContent:r,PageFooter:s,Paragraph:p},template:'<Page v-bind="$props"><slot></slot></Page>'}),parameters:{status:{type:"ALPHA"},tokens:d,tokenPrefix:"utrecht-page",docs:{component:{description:m}}}},e=l(g,{name:"Page",args:{slot:`<PageHeader>Page header</PageHeader>
+<PageContent>
+  <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+laborum.</Paragraph>
+</PageContent>
+<PageFooter>Page footer</PageFooter>`}});var n,a,o;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`createStory(meta, {
+  name: 'Page',
+  args: {
+    slot: \`<PageHeader>Page header</PageHeader>
+<PageContent>
+  <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+laborum.</Paragraph>
+</PageContent>
+<PageFooter>Page footer</PageFooter>\`
+  }
+})`,...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const G=["Default"];export{e as Default,G as __namedExportsOrder,g as default};
+//# sourceMappingURL=Page.stories-d365fecd.js.map
