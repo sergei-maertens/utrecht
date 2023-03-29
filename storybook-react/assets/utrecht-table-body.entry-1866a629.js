817a7892e8e0b805716b4098e07c1211dda3b4a4
Robbert <mail@robbertbroersma.nl>
2023-03-29 14:56:15 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@4e3da020aca2129b81adc4854ea7ef912e3dc958 🚀

--- storybook-react/assets/utrecht-table-body.entry-1866a629.js
+++
@@ -1,2 +0,0 @@
-import{r,h as t,H as s}from"./index-a2c8884e-24896750.js";import"./preload-helper-41c905a7.js";const e=":host{display:table-row-group}",l=class{constructor(o){r(this,o)}render(){return t(s,{role:"rowgroup"},t("slot",null))}};l.style=e;export{l as utrecht_table_body};
-//# sourceMappingURL=utrecht-table-body.entry-1866a629.js.map
--- storybook-react/assets/utrecht-table-footer.entry-e2f0e9ba.js
+++
@@ -1,2 +0,0 @@
-import{r,h as t,H as e}from"./index-a2c8884e-24896750.js";import"./preload-helper-41c905a7.js";const s=":host{display:table-footer-group}",l=class{constructor(o){r(this,o)}render(){return t(e,{role:"rowgroup"},t("slot",null))}};l.style=s;export{l as utrecht_table_footer};
-//# sourceMappingURL=utrecht-table-footer.entry-e2f0e9ba.js.map
