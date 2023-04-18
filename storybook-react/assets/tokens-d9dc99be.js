69e31601a97e3eaf71ff992d528b093ab5ad87a9
Robbert <mail@robbertbroersma.nl>
2023-04-18 17:06:56 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@55fc26cbb57bf3bce1a252f70d6612761ef92b98 🚀

---
+++ storybook-react/assets/tokens-d9dc99be.js
@@ -0,0 +1,2 @@
+const t={"badge-data":{"letter-spacing":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"text-transform":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"inherit | uppercase",inherits:!0}}}}},e={utrecht:t};export{e as t};
+//# sourceMappingURL=tokens-d9dc99be.js.map
