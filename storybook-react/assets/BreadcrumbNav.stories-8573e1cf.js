214643ba8cd2108cab596a0d67734395ccffe1d0
Robbert <mail@robbertbroersma.nl>
2023-04-18 17:04:54 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@860727a90817333c6e1e497c202b6e838499a929 🚀

---
+++ storybook-react/assets/BreadcrumbNav.stories-8573e1cf.js
@@ -0,0 +1,75 @@
+import{a as l,F as m,j as e}from"./jsx-runtime-670450c2.js";import"./chunk-PCJTTTQV-8a033c10.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{D as p,P as c,A as g,a as u,S as h}from"./index-d13ef923.js";import{k as b}from"./index-e825035f.js";import{t as k,d as y}from"./util-cec01095.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./preload-helper-41c905a7.js";import"./react-18-2bb9dade.js";import"./index-96c5f47c.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-0e33defa.js";import"./index-d475d2ea.js";import"./memoizerific-a8c4a000.js";import"./index-1e419f15.js";import"./chunk-RDJSMFWU-eb6ed6e5.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-Y5O7ZP4P-195fbef2.js";import"./index-d37d4223.js";import"./extends-98964cd2.js";import"./getPrototypeOf-5b183829.js";import"./index-b3b2312b.js";import"./isSymbol-69229716.js";import"./index-356e4a49.js";import"./clsx.m-b4f9149a.js";import"./index-4d501b15.js";const v=`<!-- @license CC0-1.0 -->
+
+# Breadcrumb navigation
+
+Breadcrumb navigation, ook wel "kruimelpad" genoemd, gebruik je als de pagina's van je site georganiseerd zijn in een boomstructuur. Als je site teveel pagina's heeft om naar elke pagina een link te maken op de homepage bijvoorbeeld. De eerste link is altijd naar de homepage.
+
+Het kruimelpad moet alleen links bevatten naar hogere niveau's of eerdere stappen. De pagina zelf is niet onderdeel van het kruimelpad. Gebruik geen breadcrumb navigation op de homepage, want die heeft geen hogere niveaus.
+
+## HTML
+
+Gebruik een \`nav\` element, want het is handig dat het kruimelpad een _navigation landmark_ is. Het kruimelpad is zelden de enige _navigation landmark_ op de pagina en landmarks moeten wel duidelijk te onderscheiden zijn. Gebruik daarom \`aria-label\` of \`aria-labelledby\` met een duidelijk label.
+
+Gebruik een heading element met \`aria-labelledby\`, zodat het label zichtbaar is wanneer de CSS niet geladen kan worden. Zonder stijl is het kruimelpad niet duidelijk herkenbaar, dan maakt de heading dat duidelijk.
+
+De _breadcrumb navigation_ is niet een onderdeel dat opgezocht wordt via heading navigatie van een _screen reader_. Verberg de heading met \`aria-hidden="true"\` zodat de headings uit de main content sneller te vinden zijn.
+
+Bijvoorbeeld:
+
+\`\`\`html
+<nav aria-labelledby="breadcrumb-heading">
+  <h2 id="breadcrumb-heading" aria-hidden="true">Kruimelpad</h2>
+  ...kruimelpad...
+</nav>
+\`\`\`
+
+Gebruik \`rel\` om duidelijk te maken wat het doel is van de links. ([WCAG eis 1.3.6](https://www.w3.org/TR/WCAG21/#identify-purpose)). Gebruik [\`rel="home"\`](https://microformats.org/wiki/rel-home) voor de link naar de homepage (deze relatie is nog slechts een voorstel). Gebruik [\`rel="up"\`](https://microformats.org/wiki/rel-up) voor pagina's die één niveau hoger zijn in een hierarchie. Gebruik \`rel="first"\` voor de eerste pagina in een serie.
+
+## Hoe het niet moet
+
+### Navigatie in de verkeerde _landmark_
+
+Plaats het \`<nav>\` HTML-element met de breadcrumb navigation niet binnen de _main page content_, ofwel het \`<main>\` HTML-element.
+
+Niet:
+
+\`\`\`html
+<main>
+  <nav class="breadcrumbs">
+    <a href="/">Home</a> /
+    <a href="/contact" aria-current="page">Contact</a>
+  </nav>
+  <h1>Contact</h1>
+</main>
+\`\`\`
+
+Wel:
+
+\`\`\`html
+<nav class="breadcrumbs">
+  <a href="/">Home</a> /
+  <a href="/contact" aria-current="page">Contact</a>
+</nav>
+<main>
+  <h1>Contact</h1>
+</main>
+\`\`\`
+
+## Related info
+
+- [Google Search Central - Advanced SEO - Breadcrumb](https://developers.google.com/search/docs/data-types/breadcrumb)
+- [Homepage Links Remain a Necessity — Nielsen Norman Group](https://www.nngroup.com/articles/homepage-links/)
+- [Breadcrumbs — Adobe Spectrum Design System](https://spectrum.adobe.com/page/breadcrumbs/)
+- [Exploring Markup for Breadcrumbs — Chris Coyier](https://css-tricks.com/markup-for-breadcrumbs/)
+
+## Relevante WCAG regels
+
+- [WCAG eis 1.3.1](https://www.w3.org/TR/WCAG21/#info-and-relationships): gebruik het \`<nav>\` element voor de \`navigation\` landmark role.
+- [WCAG eis 1.3.6](https://www.w3.org/TR/WCAG21/#identify-purpose)
+- [WCAG eis 2.4.5](https://www.w3.org/TR/WCAG21/#multiple-ways)
+- [WCAG eis 2.4.6](https://www.w3.org/TR/WCAG21/#headings-and-labels): de label van de \`navigation\` landmark maakt duidelijk dat het om het broodkruimelpad gaat.
+- [WCAG eis 2.4.8](https://www.w3.org/TR/WCAG21/#location):
+  - gebruik \`<a aria-current="location">\` om de huidige pagina te markeren
+  - een kruimelpad is een manier om duidelijk te maken waar de gebruiker zich bevind in de context van een site met meerdere pagina's.
+`,w={breadcrumb:{"block-size":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"font-family":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"*",inherits:!0}}},"font-size":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"text-transform":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"inherit | none | uppercase",inherits:!0}}},divider:{content:{$extensions:{"nl.nldesignsystem.css.property":{syntax:"*",inherits:!0}}},"inline-size":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}}},item:{"padding-block-start":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-block-end":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-inline-end":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-inline-start":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}}},link:{"background-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},color:{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},focus:{"background-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},color:{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}}}}}},x={utrecht:w},f={title:"React Component/Breadcrumb navigation",id:"react-breadcrumb-nav",component:b,args:{appearance:"arrows",label:"Kruimelpad",items:[{href:"/",rel:"home",index:0,children:"Home"},{href:"/a/",index:1,children:"Niveau 1"},{href:"/a/b/",rel:"up",index:2,children:"Niveau 2"}]},argTypes:{items:{description:"Links",type:{name:"array",required:!0}},appearance:{description:"Appearance",control:{type:"select"},options:["","arrows"]}},parameters:{tokensPrefix:"utrecht-breadcrumb-nav",tokens:k,tokensDefinition:x,docs:{page:()=>l(m,{children:[e(p,{children:v}),e(c,{}),e(g,{story:u}),e(h,{})]})}}},n={},r=y(f);var t,a,i;n.parameters={...n.parameters,docs:{...(t=n.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(i=(a=n.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};var s,o,d;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:"designTokenStory(meta)",...(d=(o=r.parameters)==null?void 0:o.docs)==null?void 0:d.source}}};const Q=["Default","DesignTokens"];export{n as Default,r as DesignTokens,Q as __namedExportsOrder,f as default};
+//# sourceMappingURL=BreadcrumbNav.stories-8573e1cf.js.map
---
+++ storybook-react/assets/SkipLink.stories-8430a945.js
@@ -0,0 +1,74 @@
+import{a as p,F as k,j as e}from"./jsx-runtime-670450c2.js";import"./chunk-PCJTTTQV-8a033c10.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{D as m,P as g,A as c,a as u,S as h}from"./index-d13ef923.js";import{ac as b}from"./index-e825035f.js";import{t as v,d as y}from"./util-cec01095.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./preload-helper-41c905a7.js";import"./react-18-2bb9dade.js";import"./index-96c5f47c.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-0e33defa.js";import"./index-d475d2ea.js";import"./memoizerific-a8c4a000.js";import"./index-1e419f15.js";import"./chunk-RDJSMFWU-eb6ed6e5.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-Y5O7ZP4P-195fbef2.js";import"./index-d37d4223.js";import"./extends-98964cd2.js";import"./getPrototypeOf-5b183829.js";import"./index-b3b2312b.js";import"./isSymbol-69229716.js";import"./index-356e4a49.js";import"./clsx.m-b4f9149a.js";import"./index-4d501b15.js";const j=`<!-- @license CC0-1.0 -->
+
+# Skip link
+
+_Skip links_ zijn belangrijk om te gebruiken op de meeste websites omdat voor sommige gebruikers het hierdoor niet onnodig veel tijd kost om je website te gebruiken. Met een _skip link_ ga je naar een andere plek op dezelfde pagina, zodat je bijvoorbeeld gelijk een artikel kan lezen en links daarin kunt aanklikken, zonder tijd te besteden aan de hoofdnavigatie.
+
+_Skip links_ zijn onzichtbaar als je de pagina opent, maar ze zijn wel makkelijk vindbaar voor deze gebruikers. Met het toetsenbord vindt je een _skip link_ door op \`Tab\` te drukken wanneer je de pagina hebt geladen. (Voor macOS gebruikers is het nodig om de standaard-instellingen aan te passen voordat dit werkt.)
+
+Maak skip links naar de drie belangrijkste onderdelen op de pagina, in deze volgorde:
+
+1. _main content_: vaak een link naar een \`<main>\` of \`<article>\` element
+2. _main navigation_: als je een _main navigation component_ hebt, link dan hier naar als je het niet al de volgende link is in de tabvolgorde.
+3. _search form_: als je een zoekveld hebt om vanuit deze pagina de zoeken, gebruik deze _skip link_ als laatste.
+
+Als je de skip link gaat gebruiken op een website, begin dan consistent elke pagina met de skip link zodat de navigatie op een voorspelbare manier werkt ([WCAG 3.2.3](https://www.w3.org/TR/WCAG21/#consistent-navigation)).
+
+Skip links zijn niet nodig op een simpele pagina waar alleen tekst staat en geen navigatie. Een startpagina van een zoekmachine heeft bijvoorbeeld wel een navigatie én een zoekformulier, maar door het gebruik van \`autofocus\` voor het zoekveld heeft het geen zin om een skip link te gebruiken.
+
+## HTML
+
+- plaats de _skip link_ als allereerste interactieve element van de pagina, bijvoorbeeld als eerste HTML-element in \`<body>\`.
+- plaats de _skip link_ voor landmarks zoals het \`<header>\` element van de _page header component_.
+- gebruik een \`<div>\` of \`<p>\` om de link in de plaatsen, gebruik niet een losse \`<a>\`, zodat de link ook duidelijk is als CSS niet geladen is.
+- gebruik geen \`<nav>\` element voor skip links.
+
+## Hoe het niet moet
+
+### Skip links onnodig in een \`<nav>\` HTML-element
+
+Het is belangrijk dan gebruikers snel bij de _navigation landmark_ voor het hoofdmenu komen kunnen komen, het zou niet handig zijn als je altijd eerst de _navigation landmark_ met skip links moet overslaan.
+
+Niet:
+
+\`\`\`html
+<nav class="skip-links">…</nav>
+\`\`\`
+
+Wel:
+
+\`\`\`html
+<div class="skip-links">…</div>
+\`\`\`
+
+De pagina is beter leesbaar wanneer de CSS niet is geladen als je een _block-level_ HTML-element gebruikt zoals \`div\` of \`p\` met daarin de skip links, dan komt de HTML die volgt na de skip links netjes op een eigen regel te staan.
+
+### Skip link onleesbaar door overlap met achtergrond
+
+Als je de skip link een vaste locatie geeft met CSS \`position\`, dan kan de link over de inhoud van de pagina heen staan. Gebruik daarom een achtergrondkleur voor de _skip link_, zodat de link-tekst altijd leesbaar is.
+
+### Onnodig scrollen bij focus
+
+Niet:
+
+\`\`\`css
+.utrecht-skip-link {
+  position: absolute;
+  /* … */
+}
+\`\`\`
+
+Wel:
+
+\`\`\`css
+.utrecht-skip-link {
+  position: fixed;
+  /* … */
+}
+\`\`\`
+
+## Links
+
+- [WCAG 3.2.3](https://www.w3.org/TR/WCAG21/#consistent-navigation)
+`,_={"skip-link":{"background-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},color:{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"min-block-size":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"min-inline-size":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-block-start":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-block-end":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-inline-start":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"padding-inline-end":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"text-decoration":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"none | underline",inherits:!0}}},"z-index":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<number>",inherits:!1}},focus:{"text-decoration":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"none | underline",inherits:!0}}}}}}},x={utrecht:_},f=d=>e("div",{style:{minHeight:"240px",minWidth:"320px",position:"relative",willChange:"transform"},children:d()}),z={title:"React Component/Skip link",id:"react-skip-link",component:b,argTypes:{href:{name:"href",type:{name:"string",required:!0},table:{category:"HTML attribute",defaultValue:{summary:""}}}},decorators:[f],args:{href:"#main",children:"Skip to main content"},parameters:{pseudo:{focus:!0,focusVisible:!0},tokensPrefix:"utrecht-skip-link",tokens:v,tokensDefinition:x,docs:{page:()=>p(k,{children:[e(m,{children:j}),e(g,{}),e(c,{story:u}),e(h,{})]})}}},n={},i=y(z);var t,s,a;n.parameters={...n.parameters,docs:{...(t=n.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(a=(s=n.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var o,r,l;i.parameters={...i.parameters,docs:{...(o=i.parameters)==null?void 0:o.docs,source:{originalSource:"designTokenStory(meta)",...(l=(r=i.parameters)==null?void 0:r.docs)==null?void 0:l.source}}};const Z=["Default","DesignTokens"];export{n as Default,i as DesignTokens,Z as __namedExportsOrder,z as default};
+//# sourceMappingURL=SkipLink.stories-8430a945.js.map
