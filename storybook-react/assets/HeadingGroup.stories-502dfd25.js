36e537cf81b3d6aecbfb93fdfb0f82b19ba0e5f0
Robbert <mail@robbertbroersma.nl>
2023-04-18 17:08:30 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@66f6ee758d5f0d984479ddd67ca49932acf84aad 🚀

---
+++ storybook-react/assets/HeadingGroup.stories-502dfd25.js
@@ -0,0 +1,24 @@
+import{j as e,a,F as t}from"./jsx-runtime-670450c2.js";import"./chunk-PCJTTTQV-8a033c10.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{D as k,P as H,A as w,a as x,S as D}from"./index-d13ef923.js";import{V as l,H as T,P as v,W as f}from"./index-f6961454.js";import{t as P,d as C}from"./util-cec01095.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./preload-helper-41c905a7.js";import"./react-18-2bb9dade.js";import"./index-96c5f47c.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-0e33defa.js";import"./index-d475d2ea.js";import"./memoizerific-a8c4a000.js";import"./index-1e419f15.js";import"./chunk-RDJSMFWU-eb6ed6e5.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-Y5O7ZP4P-195fbef2.js";import"./index-d37d4223.js";import"./extends-98964cd2.js";import"./getPrototypeOf-5b183829.js";import"./index-b3b2312b.js";import"./isSymbol-69229716.js";import"./index-356e4a49.js";import"./clsx.m-b4f9149a.js";import"./index-4d501b15.js";const S=`<!-- @license CC0-1.0 -->
+
+# Heading group
+
+De heading group bevat twee onderdelen:
+
+1. een heading component
+2. een pre-heading component of een paragraph component
+
+## Relevante WCAG eisen
+
+- [WCAG eis 1.3.1](https://www.w3.org/TR/WCAG21/#info-and-relationships): gebuik het \`<hgroup>\` HTML-element, met daarin een \`<h1>\`-\`<h6>\` en daarna een \`<p>\` element.
+- [WCAG eis 1.3.2](https://www.w3.org/TR/WCAG21/#meaningful-sequence): plaats HTML van heading vóór de HTML van de pre-heading, de volgorde kan daarna met CSS aangepast worden.
+`,G={"heading-group":{"margin-block-end":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"margin-block-start":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}}}},L={utrecht:G},A={title:"React Component/Heading Group",id:"react-heading-group",component:l,decorators:[y=>e("div",{style:{"--utrecht-space-around":1},children:y()})],args:{children:a(t,{children:[e(T,{children:"The Quick Brown Fox Jumps Over The Lazy Dog"}),e(v,{children:"The Quick Brown Fox Jumps Over The Lazy Dog"})]})},parameters:{tokensPrefix:"utrecht-heading-group",tokens:P,tokensDefinition:L,docs:{page:()=>a(t,{children:[e(k,{children:S}),e(H,{}),e(w,{story:x}),e(D,{})]})}}},r={},n={args:{children:a(l,{style:{"--utrecht-space-around":1},children:[e(T,{children:"The Quick Brown Fox Jumps Over The Lazy Dog"}),e(f,{children:"The Quick Brown Fox Jumps Over The Lazy Dog"})]})}},o=C(A);var s,i,p;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var m,d,c;n.parameters={...n.parameters,docs:{...(m=n.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: <HeadingGroup style={({
+      '--utrecht-space-around': 1
+    } as any)}>
+        <Heading1>The Quick Brown Fox Jumps Over The Lazy Dog</Heading1>
+        <PreHeading>The Quick Brown Fox Jumps Over The Lazy Dog</PreHeading>
+      </HeadingGroup>
+  }
+}`,...(c=(d=n.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var g,h,u;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:"designTokenStory(meta)",...(u=(h=o.parameters)==null?void 0:h.docs)==null?void 0:u.source}}};const te=["Default","WithPreHeading","DesignTokens"];export{r as Default,o as DesignTokens,n as WithPreHeading,te as __namedExportsOrder,A as default};
+//# sourceMappingURL=HeadingGroup.stories-502dfd25.js.map
---
+++ storybook-react/assets/RadioButton.stories-83376c4b.js
@@ -0,0 +1,54 @@
+import{a as B,F as O,j as s}from"./jsx-runtime-670450c2.js";import"./chunk-PCJTTTQV-8a033c10.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{D as Y,P as z,A as E,a as J,S as K}from"./index-d13ef923.js";import{a8 as G,K as N,J as Q}from"./index-f6961454.js";import{t as U,d as X}from"./util-cec01095.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./preload-helper-41c905a7.js";import"./react-18-2bb9dade.js";import"./index-96c5f47c.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-0e33defa.js";import"./index-d475d2ea.js";import"./memoizerific-a8c4a000.js";import"./index-1e419f15.js";import"./chunk-RDJSMFWU-eb6ed6e5.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-Y5O7ZP4P-195fbef2.js";import"./index-d37d4223.js";import"./extends-98964cd2.js";import"./getPrototypeOf-5b183829.js";import"./index-b3b2312b.js";import"./isSymbol-69229716.js";import"./index-356e4a49.js";import"./clsx.m-b4f9149a.js";import"./index-4d501b15.js";const Z=`<!-- @license CC0-1.0 -->
+
+# Radio button
+
+## Terminologie
+
+- **radio button**: "Radio button" heeft [een Wikipedia-lemma](https://en.wikipedia.org/wiki/Radio_button), [WAI-ARIA Authoring Practices 1.2](https://www.w3.org/TR/wai-aria-practices-1.2/#radiobutton) noemt het ook "radio button" en "radio group".
+- **checked**: [WAI-ARIA Authoring Practices 1.2](https://www.w3.org/TR/wai-aria-practices-1.2/#radiobutton) en HTML noemen de staat van de radio button "checked", CSS heeft de \`:checked\` pseudo-class.
+
+## Class names
+
+- \`.utrecht-radio-button\`
+`,ee={"radio-button":{"background-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"border-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"border-width":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},size:{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"margin-block-start":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},active:{"border-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"border-width":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"background-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},color:{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}}},checked:{"border-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"border-width":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"background-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},color:{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}}},hover:{"border-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"border-width":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"background-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},color:{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}}},focus:{"border-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"border-width":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"background-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},color:{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}}},disabled:{"border-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"border-width":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"background-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},color:{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}}},invalid:{"border-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"border-width":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"background-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},color:{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}}}}},se={utrecht:ee},re={title:"React Component/Radio Button",id:"react-radio-button",component:G,subcomponents:{FormLabel:N,FormField:Q},parameters:{tokensPrefix:"utrecht-radio-button",tokens:U,tokensDefinition:se,docs:{page:()=>B(O,{children:[s(Y,{children:Z}),s(z,{}),s(E,{story:J}),s(K,{})]})}},argTypes:{checked:{name:"checked",type:{name:"boolean",required:!1},table:{defaultValue:{summary:!1},category:"HTML attribute"}},disabled:{name:"disabled",type:{name:"boolean",required:!1},table:{defaultValue:{summary:!1},category:"HTML attribute"}},invalid:{name:"invalid",type:{name:"boolean",required:!1},table:{defaultValue:{summary:!1}}},required:{name:"required",type:{name:"boolean",required:!1},table:{defaultValue:{summary:!1},category:"HTML attribute"}},value:{name:"value",type:{name:"string",required:!1},table:{category:"HTML attribute"}}}},r={},t={parameters:{pseudo:{hover:!0}}},o={parameters:{pseudo:{focus:!0}}},n={parameters:{pseudo:{focusVisible:!0}}},e={args:{checked:!0,value:"checked"}};e.args={checked:!0,value:"checked"};const a={args:{checked:!0,disabled:!0}},i={args:{disabled:!0}},c={args:{invalid:!0}},d={args:{required:!0}},l=X(re);var p,u,m;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(m=(u=r.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};var y,g,h;t.parameters={...t.parameters,docs:{...(y=t.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      hover: true
+    }
+  }
+}`,...(h=(g=t.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};var b,x,k;o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      focus: true
+    }
+  }
+}`,...(k=(x=o.parameters)==null?void 0:x.docs)==null?void 0:k.source}}};var f,$,v;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  parameters: {
+    pseudo: {
+      focusVisible: true
+    }
+  }
+}`,...(v=($=n.parameters)==null?void 0:$.docs)==null?void 0:v.source}}};var w,S,R;e.parameters={...e.parameters,docs:{...(w=e.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    checked: true,
+    value: 'checked'
+  }
+}`,...(R=(S=e.parameters)==null?void 0:S.docs)==null?void 0:R.source}}};var T,A,q;a.parameters={...a.parameters,docs:{...(T=a.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    checked: true,
+    disabled: true
+  }
+}`,...(q=(A=a.parameters)==null?void 0:A.docs)==null?void 0:q.source}}};var D,C,F;i.parameters={...i.parameters,docs:{...(D=i.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(F=(C=i.parameters)==null?void 0:C.docs)==null?void 0:F.source}}};var V,H,I;c.parameters={...c.parameters,docs:{...(V=c.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    invalid: true
+  }
+}`,...(I=(H=c.parameters)==null?void 0:H.docs)==null?void 0:I.source}}};var L,M,P;d.parameters={...d.parameters,docs:{...(L=d.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    required: true
+  }
+}`,...(P=(M=d.parameters)==null?void 0:M.docs)==null?void 0:P.source}}};var _,j,W;l.parameters={...l.parameters,docs:{...(_=l.parameters)==null?void 0:_.docs,source:{originalSource:"designTokenStory(meta)",...(W=(j=l.parameters)==null?void 0:j.docs)==null?void 0:W.source}}};const Fe=["Default","Hover","Focus","FocusVisible","Checked","CheckedAndDisabled","Disabled","Invalid","Required","DesignTokens"];export{e as Checked,a as CheckedAndDisabled,r as Default,l as DesignTokens,i as Disabled,o as Focus,n as FocusVisible,t as Hover,c as Invalid,d as Required,Fe as __namedExportsOrder,re as default};
+//# sourceMappingURL=RadioButton.stories-83376c4b.js.map
