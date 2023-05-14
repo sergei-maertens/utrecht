a9e13f80b5ab681805f0f6ab8b09d44def4a4946
Robbert <mail@robbertbroersma.nl>
2023-05-14 15:12:49 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@830eaaa00ded796e8edfa96523360b3e29e4bc3b 🚀

---
+++ storybook-vue/assets/Heading1.stories-a01aa5ea.js
@@ -0,0 +1,7 @@
+import{R as a,c as s}from"./util-592fa0dc.js";import{t as n}from"./tokens-7a692d05.js";import"./vue.esm-bundler-73faf489.js";import"./chunk-PCJTTTQV-e255455a.js";import"./iframe-03713653.js";import"../sb-preview/runtime.mjs";import"./_commonjsHelpers-725317a4.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-e90b8e62.js";import"./index-356e4a49.js";import"./chunk-R4NKYYJA-96bb58e6.js";const m={id:"vue-heading-1",title:"Vue.js Component/Heading 1",component:a,tags:["autodocs"],argTypes:{},args:{},parameters:{status:{type:"ALPHA"},tokens:n,tokenPrefix:"utrecht-heading-1",docs:{source:{code:'<UtrechtHeading1 v-bind="$props"><slot></slot></UtrechtHeading1>'}}}},t=s(m,{name:"Heading 1",args:{textContent:"The Quick Brown Fox Jumps Over The Lazy Dog"}});var e,o,r;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`createStory(meta, {
+  name: 'Heading 1',
+  args: {
+    textContent: 'The Quick Brown Fox Jumps Over The Lazy Dog'
+  }
+})`,...(r=(o=t.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const T=["Default"];export{t as Default,T as __namedExportsOrder,m as default};
+//# sourceMappingURL=Heading1.stories-a01aa5ea.js.map
