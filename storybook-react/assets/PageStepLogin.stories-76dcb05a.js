b7638ca2f1ef26fbd281349ae20d2e5a7977142a
Robbert <mail@robbertbroersma.nl>
2023-05-14 15:14:04 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@d48bc0de6ad7a2c3228cdd69393641c41a9c3a5d 🚀

---
+++ storybook-react/assets/PageStepLogin.stories-76dcb05a.js
@@ -0,0 +1,41 @@
+import{a as t,j as e}from"./jsx-runtime-91a467a5.js";import{f as d,g as p,L as r,h as l,j as h,P as i,B as m,S as n,k as u,n as w}from"./index.esm-ca190a96.js";import{e as D,f as k,g as f,h as o}from"./index.esm-4b1221cc.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./defineProperty-a128c648.js";import"./clsx.m-1229b3e0.js";import"./getPrototypeOf-5b183829.js";import"./index-8ce4a492.js";import"./iframe-8ac03048.js";import"../sb-preview/runtime.mjs";const O={title:"Template/DigiD login page",id:"template-form-pages-step-login",component:d},b=()=>t(d,{children:[e(p,{children:e(r,{href:"/",children:e(D,{})})}),t(l,{style:{"--utrecht-space-around":1},children:[e(k,{json:JSON.stringify([{href:"https://example/",title:"Home",current:!1},{href:"https://example/",title:"Online loket",current:!0}]),variant:"arrows"}),e(h,{children:"Inloggen"}),e(i,{children:"Log in met uw DigiD om verder to gaan"}),e(f,{children:t(m,{appearance:"primary-action-button",children:["Inloggen met DigiD ",e(o,{})]})}),e(n,{}),t("aside",{children:[e(u,{children:"Uitleg over DigiD?"}),t(i,{children:["Vindt u DigiD ingewikkeld? Bekijk dan deze",e(r,{href:"https://www.uabc.nl/?osc=uabc",children:"uitleg over DigiD."})]})]}),e(n,{}),t(r,{external:!0,href:"https://www.kcmsurvey.com/qSwudd733b9c27c2e91ba8c7b598MaSd?webpagina=Inloggen",children:[e(o,{})," Wat vindt u van deze pagina?"]})]}),e(w,{})]}),a=b.bind({});var g,c,s;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`() => <Page>
+    <PageHeader>
+      <Link href="/">
+        <UtrechtLogo />
+      </Link>
+    </PageHeader>
+    <PageContent style={({
+    '--utrecht-space-around': 1
+  } as any)}>
+      <UtrechtBreadcrumb json={JSON.stringify([{
+      href: 'https://example/',
+      title: 'Home',
+      current: false
+    }, {
+      href: 'https://example/',
+      title: 'Online loket',
+      current: true
+    }])} variant="arrows" />
+      <Heading1>Inloggen</Heading1>
+      <Paragraph>Log in met uw DigiD om verder to gaan</Paragraph>
+      <UtrechtDigidButton>
+        <ButtonLink appearance="primary-action-button">
+          Inloggen met DigiD <UtrechtIconArrow />
+        </ButtonLink>
+      </UtrechtDigidButton>
+      <Separator />
+      <aside>
+        <Heading2>Uitleg over DigiD?</Heading2>
+        <Paragraph>
+          Vindt u DigiD ingewikkeld? Bekijk dan deze
+          <Link href="https://www.uabc.nl/?osc=uabc">uitleg over DigiD.</Link>
+        </Paragraph>
+      </aside>
+      <Separator />
+      <Link external href="https://www.kcmsurvey.com/qSwudd733b9c27c2e91ba8c7b598MaSd?webpagina=Inloggen">
+        <UtrechtIconArrow /> Wat vindt u van deze pagina?
+      </Link>
+    </PageContent>
+    <PageFooter />
+  </Page>`,...(s=(c=a.parameters)==null?void 0:c.docs)==null?void 0:s.source}}};const z=["Login"];export{a as Login,z as __namedExportsOrder,O as default};
+//# sourceMappingURL=PageStepLogin.stories-76dcb05a.js.map
