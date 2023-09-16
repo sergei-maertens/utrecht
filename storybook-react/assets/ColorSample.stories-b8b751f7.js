7f27abd6698dcb83f3fed186968bb55e8605db8a
Robbert <mail@robbertbroersma.nl>
2023-09-16 13:29:07 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@c4cfe289241adfa9dea35a76a6d34c3523ec9e9a 🚀

---
+++ storybook-react/assets/ColorSample.stories-b8b751f7.js
@@ -0,0 +1,29 @@
+import{a as d,F as c,j as e}from"./jsx-runtime-29545a09.js";import"./chunk-S4VUQJ4A-ffc841d1.js";import{D as m,P as p,A as g,b as u,d as k}from"./index-4b85beca.js";import{a5 as b}from"./index-db302ede.js";import{t as h,d as y}from"./util-7fc75af6.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-de5b2a35.js";import"../sb-preview/runtime.js";import"./react-18-988a5df2.js";import"./index-d3ea75b5.js";import"./index-d37d4223.js";import"./extends-98964cd2.js";import"./getPrototypeOf-a3ec403f.js";import"./index-afe93f9d.js";import"./index-356e4a49.js";import"./defineProperty-75c483e5.js";import"./clsx.m-1229b3e0.js";import"./index-8d47fad6.js";const f=`<!-- @license CC0-1.0 -->
+
+# Color sample
+
+Gebruik _color sample_ component om een voorbeeld te geven van een kleur. Gebruik deze component bijvoorbeeld voor:
+
+- Legenda van de kleuren in landkaarten, grafieken of van data badges.
+- Overzicht van kleuren in een huisstijl.
+- Kleur-optie in een _color input_ component.
+
+## Toegankelijkheid
+
+### Tekstlabels voor kleuren
+
+Gebruik ook een tekstlabel om de kleur te beschrijven. Bijvoorbeeld: "rood", "lichtrood", etcetera.
+
+Een gebruiker die geen kleuren ziet kan het met een andere gebruiker over de "rode lijn" hebben, als het tekstlabel goed is. Vervang daarom de kleur in het tekstlabel niet door de bedoeling van de kleur: "hoog water", "laag water". Combineer de kleur met de bedoeling: "blauw, hoog water" en "geel, laag water".
+
+### Forced colors mode
+
+Bij sommige gebruikers worden kleuren in CSS zoals \`background-color\` vervangen of genegeerd, omdat ze een bepaald kleurenpalet nodig hebben. Voor datavisualisatie-kleuren kan dit functionaliteit kapot maken, terwijl voor decoratieve huisstijl kleuren het meestal geen probleem is.
+
+Gebruik \`forced-color-adjust: none\` om te voorkomen dat de kleur wordt aangepast, zodat de betekenis duidelijk blijft. Lees meer hierover op deze Microsoft blog post: [Styling for Windows high contrast with new standards for forced colors](https://blogs.windows.com/msedgedev/2020/09/17/styling-for-windows-high-contrast-with-new-standards-for-forced-colors/).
+
+## WCAG eisen
+
+- [WCAG eis 3.2.4](https://www.w3.org/TR/WCAG21/#consistent-identification): gebruik hetzelfde label voor de kleur in de _color sample_ van de legenda als in de landkaart of grafiek.
+`,v={"color-sample":{"background-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"border-width":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"border-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}},"border-radius":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"block-size":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},"inline-size":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<length>",inherits:!0}}},dark:{"border-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}}},light:{"border-color":{$extensions:{"nl.nldesignsystem.css.property":{syntax:"<color>",inherits:!0}}}}}},w={utrecht:v},x={title:"React Component/Color sample",id:"react-color-sample",component:b,args:{color:"#007DAD"},argTypes:{color:{name:"Color",control:{type:"color"},type:{name:"string",required:!0},table:{category:"API"}}},parameters:{tokensPrefix:"utrecht-color-sample",tokens:h,tokensDefinition:w,docs:{page:()=>d(c,{children:[e(m,{markdown:f}),e(p,{}),e(g,{story:u}),e(k,{})]})}}},o={},r=y(x);var n,t,s;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(s=(t=o.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};var i,a,l;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:"designTokenStory(meta)",...(l=(a=r.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};const M=["Default","DesignTokens"];export{o as Default,r as DesignTokens,M as __namedExportsOrder,x as default};
+//# sourceMappingURL=ColorSample.stories-b8b751f7.js.map
--- storybook-vue/assets/preview-7886cbf3.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-fdfc0196.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-93b67942.js"),["./DocsRenderer-3PUGWF3O-93b67942.js","./chunk-S4VUQJ4A-33d40054.js","./iframe-fdfc0196.js","./_commonjsHelpers-725317a4.js","./index-d37d4223.js","./index-8bfd3a25.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-7886cbf3.js.map
