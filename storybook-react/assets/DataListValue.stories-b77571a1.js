b3b1bbc15d8866edf5a06773175359bec0b63d59
Robbert <mail@robbertbroersma.nl>
2023-10-01 15:16:16 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@c65afd22da64dd9473ff125ec27caf9dc1076ed9 🚀

---
+++ storybook-react/assets/DataListValue.stories-b77571a1.js
@@ -0,0 +1,26 @@
+import{j as a}from"./jsx-runtime-29545a09.js";import{c as E,D as S,a as w}from"./DataList-d7663da0.js";import{c as N}from"./clsx.m-1229b3e0.js";import{r as v}from"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";const r=v.forwardRef(({children:t,className:_,...b},x)=>a("bdi",{translate:"no",...b,ref:x,className:N("utrecht-url-data",_),children:t}));r.displayName="URLData";try{r.displayName="URLData",r.__docgenInfo={description:"",displayName:"URLData",props:{}}}catch{}const W={title:"React Component/Data List/Value",id:"react-data-list--value",component:E,decorators:[t=>a(S,{appearance:"rows",children:a(w,{children:t()})})],argTypes:{multiline:{type:"boolean",table:{category:"API",defaultValue:{summary:!1}}},notranslate:{type:"boolean",table:{category:"API",defaultValue:{summary:!1}}},value:{type:"string",table:{defaultValue:{summary:void 0}}}},args:{children:"Amsterdam"}},s={},e={args:{notranslate:!0,children:"Mees de Vos"}};e.args={children:"Mees de Vos",notranslate:!0};const n={args:{children:a(r,{children:"https://example.fi/"}),notranslate:!0}},o={args:{children:a(r,{children:"mail@example.fi"}),notranslate:!0}},l={args:{children:`Tweede Kamer der Staten-Generaal
+Postbus 20018
+2500 EA Den Haag`,multiline:!0}};var i,c,d;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:"{}",...(d=(c=s.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var m,u,p;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    notranslate: true,
+    children: 'Mees de Vos'
+  }
+}`,...(p=(u=e.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};var g,h,f;n.parameters={...n.parameters,docs:{...(g=n.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    // Perhaps "voorbeeld.fi" when translated from English to Dutch
+    // "fi" is used so we can test rendering of ligatures is avoided
+    children: <URLData>https://example.fi/</URLData>,
+    notranslate: true
+  }
+}`,...(f=(h=n.parameters)==null?void 0:h.docs)==null?void 0:f.source}}};var D,y,L;o.parameters={...o.parameters,docs:{...(D=o.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    children: <URLData>mail@example.fi</URLData>,
+    notranslate: true
+  }
+}`,...(L=(y=o.parameters)==null?void 0:y.docs)==null?void 0:L.source}}};var V,R,U;l.parameters={...l.parameters,docs:{...(V=l.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    children: 'Tweede Kamer der Staten-Generaal\\nPostbus 20018\\n2500 EA Den Haag',
+    multiline: true
+  }
+}`,...(U=(R=l.parameters)==null?void 0:R.docs)==null?void 0:U.source}}};const j=["Default","NoTranslate","ValueWithURL","ValueWithEmail","MultilineValue"];export{s as Default,l as MultilineValue,e as NoTranslate,o as ValueWithEmail,n as ValueWithURL,j as __namedExportsOrder,W as default};
+//# sourceMappingURL=DataListValue.stories-b77571a1.js.map
--- storybook-vue/assets/preview-ffbab322.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-49d00fcc.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-7255709e.js"),["./DocsRenderer-3PUGWF3O-7255709e.js","./chunk-S4VUQJ4A-5361a843.js","./iframe-49d00fcc.js","./_commonjsHelpers-725317a4.js","./index-d37d4223.js","./index-8bfd3a25.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-ffbab322.js.map
