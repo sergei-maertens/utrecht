099efedeb4aece4b6a6269d07cf9aeb0ac8baa4b
Robbert <mail@robbertbroersma.nl>
2023-10-08 09:07:38 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@f9b2d3c3e32ee7ab437d1e37aa5e838180674560 🚀

---
+++ storybook-vue/assets/LinkSocial.stories-39643a17.js
@@ -0,0 +1,15 @@
+import{w as t,I as s}from"./component-library-vue-84bb6690.js";import{l as n,t as m,r as l}from"./index-7769e6d8-da0f0b26.js";import"./vue.esm-bundler-759e87b8.js";import{c as i}from"./util-d3a18a4e.js";import"./chunk-S4VUQJ4A-eeabee0d.js";import"./iframe-610fb016.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-d37d4223.js";import"./index-8bfd3a25.js";import"./index-356e4a49.js";const p={id:"vue-link-social",title:"Vue.js Component/Link Social",component:t,tags:["autodocs"],argTypes:{slot:{name:"slot",type:{name:"string",required:!1},table:{defaultValue:{summary:""}}},external:{name:"external",control:{type:"boolean"},type:"boolean",table:{defaultValue:{summary:""}}}},render:c=>({setup(){return{args:c}},components:{LinkSocial:t,UtrechtIconFacebook:n},template:'<LinkSocial v-bind="$props"><slot></slot></LinkSocial>'}),parameters:{status:{type:"ALPHA"},tokens:m,tokenPrefix:"utrecht-link-social",docs:{description:{component:l}}}},e=i(p,{name:"LinkSocial",component:t,components:{UtrechtIconFacebook:n,Image:s},args:{slot:"<UtrechtIconFacebook />",href:"https://www.facebook.com/GemeenteUtrecht",external:!0,"aria-label":"de gemeente Utrecht op Facebook"}});var o,a,r;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`createStory(meta, {
+  name: 'LinkSocial',
+  component: LinkSocial,
+  components: {
+    UtrechtIconFacebook,
+    Image
+  },
+  args: {
+    slot: \`<UtrechtIconFacebook />\`,
+    href: 'https://www.facebook.com/GemeenteUtrecht',
+    external: true,
+    'aria-label': 'de gemeente Utrecht op Facebook'
+  }
+})`,...(r=(a=e.parameters)==null?void 0:a.docs)==null?void 0:r.source}}};const w=["Default"];export{e as Default,w as __namedExportsOrder,p as default};
+//# sourceMappingURL=LinkSocial.stories-39643a17.js.map
---
+++ storybook-vue/assets/preview-02844b80.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-610fb016.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-d9d74b21.js"),["./DocsRenderer-3PUGWF3O-d9d74b21.js","./chunk-S4VUQJ4A-eeabee0d.js","./iframe-610fb016.js","./_commonjsHelpers-725317a4.js","./index-d37d4223.js","./index-8bfd3a25.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-02844b80.js.map
